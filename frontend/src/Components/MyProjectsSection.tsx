e6948cafb0b62d0d97f266ac0d9560625009a512
torisutanjon <giraotristan@gmail.com>
2022-12-30 13:44:59 +0800

    update: edited text and images in myprojects section

--- frontend/src/Components/MyProjectsSection.tsx
+++ frontend/src/Components/MyProjectsSection.tsx
@@ -29,17 +29,17 @@ const ProjectsComponent = () => {
     link: "https://github.com/torisutanjon/ecommerceapp",
   });
   const [projectTwo] = useState<Project["project"]>({
-    image: foreignExchange_image,
+    image: nftEcommerce_image,
     title: "MERN Stack Website With NextJS",
     description:
       "This is a web application for NFT Publishing. I created this project to learn MERN stack and implementing it on NextJS.",
     link: "https://github.com/torisutanjon/nft-ecommerce",
   });
   const [projectThree] = useState<Project["project"]>({
-    image: nftEcommerce_image,
+    image: foreignExchange_image,
     title: "Exchange Rate Software using Java",
     description:
-      "This is a simple desktop application that I created to refreshen my knowledge in desktop application and Java as focused on web development and Python for a long time.",
+      "This is a simple desktop application that I created to refreshen my knowledge in desktop application and Java as I was focused on web development and Python for a long time.",
     link: "https://github.com/torisutanjon/exchangerate",
   });
 
@@ -47,8 +47,8 @@ const ProjectsComponent = () => {
     <div id="my-projects">
       <div id="title">
         <p>
-          I did most of my self learning in programming by creating personal
-          projects as pracice and learn as the project goes. Here are some of
+          I did the most of my self-learning in programming by creating personal
+          projects to practice and learn as the project goes. Here are some of
           those.
         </p>
       </div>
